f696fc8aeef3e17a4c66cd6a145d6fe83a1057bc
Akriti Agarwal <akritiragrawal10@gmail.com>
2026-05-19 21:44:53 +0530

    Updated 3-4 frontend pages

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,12 +1,37 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets";
+import { createContext, useState } from "react";
+import { doctors, assets } from "../assets/assets";
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
+    const [userData, setUserData] = useState({
+        name: 'Edward Vincent',
+        image: assets.profile_pic,
+        email: 'edward@gmail.com',
+        phone: '+1 123 456 789',
+        address: {
+            line1: '57th Cross, Richmond',
+            line2: 'Circle, Church Road, London'
+        },
+        gender: 'Male',
+        dob: '2000-01-20'
+    })
+
+    const token = true
+    const backendUrl = 'http://localhost:4000'
+
+    const loadUserProfileData = async () => {
+
+    }
+
     const value = {
-        doctors
+        doctors,
+        userData,
+        setUserData,
+        token,
+        backendUrl,
+        loadUserProfileData
     }
 
     return (
