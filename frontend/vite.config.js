7bf0733b9518a79cab0f10333acfb8fc9c37d716
Akriti321 <166243384+Akriti321@users.noreply.github.com>
2026-05-30 21:52:49 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{port:5173}
 })
-
-
